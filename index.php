6f70ece56d4c5051abc576c7d66f8a2efe5dd6c5
Absaar Ali Rizwan <72137848+absaar-ali12@users.noreply.github.com>
2023-04-04 21:35:49 +0530

    home

---
+++ index.php
@@ -0,0 +1,369 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+    <title>Mindspace IT</title>
+    <meta content="description" name="Mindspace IT">
+    <meta content="" name="keywords">
+
+    <!-- Favicons -->
+    <link href="assets/img/favicon.png" rel="icon">
+    <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Montserrat:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+    <!-- Vendor CSS Files -->
+    <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+    <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+    <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+    <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+    <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+    <!-- Template Main CSS File -->
+    <link href="assets/css/style.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+
+
+    <!-- ======= Header ======= -->
+    <?php include("header.php"); ?>
+    <!-- End Header -->
+
+    <!-- ======= Hero Section ======= -->
+    <section id="hero">
+        <div class="hero-container">
+            <h1 data-aos="zoom-in">Welcome To Mindspace IT</h1>
+            <h2 data-aos="fade-up">Next Generation Mobile & Software Development to Enable businesses to Reinvent <br>The future with cutting edge digital transformation & rapid innovation</h2>
+
+        </div>
+    </section>
+    <!-- End Hero -->
+
+    <main id="main">
+
+        
+
+
+        <!-- ======= Why Us Section ======= -->
+        <section id="why-us" class="why-us">
+            <div class="container-fluid">
+
+                <div class="row">
+
+                    <div class="col-lg-8 d-flex flex-column justify-content-center ">
+
+                        <div class="content" data-aos="fade-up">
+                            <h3>MindSpace IT</h3>
+                            <p>
+                                The Perfect Partners For Mobile & Software Development Solutions to Enable Small Medium and Big Businesses to Grow Digitally</p>
+                            <p class="fst-italic">
+                                The team at Mind Space IT is committed to solving your toughest problems using high-end Technologies.<br> Whether you need help to build a website , or any software development and your IT Consulting services we have you
+                                covered. Our team offers a range of IT services for your specific needs.
+                            </p>
+                            </p>
+                        </div>
+
+                        <div class="accordion-list">
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <ul>
+                                <li data-aos="fade-up" data-aos-delay="100">
+    <a class="collapse" href="web-development.php" ><span>1</span>Web Development</a></li>
+<li data-aos="fade-up" data-aos-delay="100">
+    <a class="collapse" href="mobile-app-development.php" ><span>2</span>Mobile App Development</a></li>
+<li data-aos="fade-up" data-aos-delay="100">
+    <a class="collapse" href="software-development.php" ><span>3</span>Software Development</a></li>
+<li data-aos="fade-up" data-aos-delay="100">
+    <a class="collapse" href="it-consulting-service.php" ><span>4</span>IT Consulting Services</a></li>
+<li data-aos="fade-up" data-aos-delay="100">
+    <a class="collapse" href="qa-service.php" ><span>5</span>QA Service</a></li>
+    
+</ul>
+                                    
+                                </div>
+                                <div class="col-md-6">
+                                    <ul>
+    
+<li data-aos="fade-up" data-aos-delay="100">
+    <a class="collapse" href="devops-service.php" ><span>6</span>DevOps Service</a></li>
+<li data-aos="fade-up" data-aos-delay="100">
+    <a class="collapse" href="it-outsourcing-service.php" ><span>7</span>IT Outsourcing Service</a></li>
+<li data-aos="fade-up" data-aos-delay="100">
+    <a class="collapse" href="staff-augmentation-services" ><span>8</span>Staff Augmentation Services</a></li>
+<li data-aos="fade-up" data-aos-delay="100">
+    <a class="collapse" href="virtual-staffing.php" ><span>9</span>Virtual Staffing</a></li>
+<li data-aos="fade-up" data-aos-delay="100">
+    <a class="collapse" href="digital-marketing.php" ><span>10</span>Digital Marketing Services</a></li>
+
+                                
+
+                            </ul>
+                                    
+                                </div>
+                                
+                            </div>
+                                
+                            
+
+                 
+</ul></div>
+
+                    </div>
+
+                    <div class="col-lg-4  align-items-stretch video-box" style='background-image: url("assets/img/why-us.jpg");' data-aos="zoom-in"></div>
+
+                </div>
+
+            </div>
+        </section>
+        <!-- End Why Us Section -->
+        <!-- ======= Team Section ======= -->
+        <section id="team" class="team">
+            <div class="container">
+
+                <div class="section-title" data-aos="fade-up">
+                    <h2>Our Services</h2>
+                    
+                </div>
+
+                <div class="row justify-content-items">
+                    <div class="col-lg-3 col-md-6 d-flex ">
+<div class="member" data-aos="zoom-in">
+<div class="member-img">
+<img src="assets/img/featured-1.png" class="img-fluid" alt="">
+</div>
+<div class="member-info">
+<h4>Web Development</h4>
+<p>Mind Space IT provides end-to-end website design, web application creation, and portal development services.</p>
+<a data-aos="fade-up" data-aos-delay="200" href="web-development.php" class="btn btn-primary">Know More</a>
+</div>
+</div>
+</div>
+
+
+<div class="col-lg-3 col-md-6 d-flex ">
+<div class="member" data-aos="zoom-in">
+<div class="member-img">
+<img src="assets/img/featured-2.png" class="img-fluid" alt="">
+</div>
+<div class="member-info">
+<h4>Mobile App Development</h4>
+<p>Portable application improvement is that the demonstration or interaction by which a versatile application is produced for cell phones.</p>
+<a data-aos="fade-up" data-aos-delay="200" href="mobile-app-development.php" class="btn btn-primary">Know More</a>
+</div>
+</div>
+</div>
+
+
+<div class="col-lg-3 col-md-6 d-flex ">
+<div class="member" data-aos="zoom-in">
+<div class="member-img">
+<img src="assets/img/featured-3.png" class="img-fluid" alt="">
+</div>
+<div class="member-info">
+<h4>Software Development</h4>
+<p>We will create cost effective software design focused on your needs and the needs of your customers. </p>
+<a data-aos="fade-up" data-aos-delay="200" href="software-development.php" class="btn btn-primary">Know More</a>
+</div>
+</div>
+</div>
+
+
+<div class="col-lg-3 col-md-6 d-flex ">
+<div class="member" data-aos="zoom-in">
+<div class="member-img">
+<img src="assets/img/featured-4.png" class="img-fluid" alt="">
+</div>
+<div class="member-info">
+<h4>IT Consulting Services</h4>
+<p>Mind Space provides software innovation services that work in tandem with our global distribution platform. </p>
+<a data-aos="fade-up" data-aos-delay="200" href="it-consulting-services.php" class="btn btn-primary">Know More</a>
+</div>
+</div>
+</div>
+
+
+<div class="col-lg-3 col-md-6 d-flex ">
+<div class="member" data-aos="zoom-in">
+<div class="member-img">
+<img src="assets/img/featured-5.png" class="img-fluid" alt="">
+</div>
+<div class="member-info">
+<h4>QA Service</h4>
+<p>Innovation within the industry has resulted in an ever-increasing demand for IT testing, allowing developers to direct their efforts during the design stage. </p>
+<a data-aos="fade-up" data-aos-delay="200" href="qa-service.php" class="btn btn-primary">Know More</a>
+</div>
+</div>
+</div>
+
+
+<div class="col-lg-3 col-md-6 d-flex ">
+<div class="member" data-aos="zoom-in">
+<div class="member-img">
+<img src="assets/img/featured-6.png" class="img-fluid" alt="">
+</div>
+<div class="member-info">
+<h4>DevOps Service</h4>
+<p>. Our Devops Services help drive a culture of "Everything as Code", making concordance in cloud activities Cue rationale's. </p>
+<a data-aos="fade-up" data-aos-delay="200" href="devops-service.php" class="btn btn-primary">Know More</a>
+</div>
+</div>
+</div>
+
+
+<div class="col-lg-3 col-md-6 d-flex ">
+<div class="member" data-aos="zoom-in">
+<div class="member-img">
+<img src="assets/img/featured-7.png" class="img-fluid" alt="">
+</div>
+<div class="member-info">
+<h4>IT Outsourcing Service</h4>
+<p>IT Consulting administrations have arisen together of the premier powerful and advantageous technique for decreasing generally operational expense of a business. </p>
+<a data-aos="fade-up" data-aos-delay="200" href="it-outsourcing-service.php" class="btn btn-primary">Know More</a>
+</div>
+</div>
+</div>
+
+
+<div class="col-lg-3 col-md-6 d-flex ">
+<div class="member" data-aos="zoom-in">
+<div class="member-img">
+<img src="assets/img/featured-8.png" class="img-fluid" alt="">
+</div>
+<div class="member-info">
+<h4>Staff Augmentation Services</h4>
+<p>The right staffing can make or break a business, and we ensure you get access to the most talented and qualified industry professionals. </p>
+<a data-aos="fade-up" data-aos-delay="200" href="staff-agumentation-services.php" class="btn btn-primary">Know More</a>
+</div>
+</div>
+</div>
+
+
+<div class="col-lg-3 col-md-6 d-flex ">
+<div class="member" data-aos="zoom-in">
+<div class="member-img">
+<img src="assets/img/featured-9.png" class="img-fluid" alt="">
+</div>
+<div class="member-info">
+<h4>Virtual Staffing</h4>
+<p>Mind space IT is one the notable association for Virtual staffing administrations that give staff, resources. </p>
+<a data-aos="fade-up" data-aos-delay="200" href="virtual-staffing.php" class="btn btn-primary">Know More</a>
+</div>
+</div>
+</div>
+
+
+<div class="col-lg-3 col-md-6 d-flex ">
+<div class="member" data-aos="zoom-in">
+<div class="member-img">
+<img src="assets/img/featured-10.png" class="img-fluid" alt="">
+</div>
+<div class="member-info">
+<h4>Digital Marketing Services</h4>
+<p>Mind Space IT offers kinds of help starting from looking to substance to electronic media to flexible publicizing and help brands with upgrading their online presence. </p>
+<a data-aos="fade-up" data-aos-delay="200" href="digital-marketing.php" class="btn btn-primary">Know More</a>
+</div>
+</div>
+</div>
+
+       
+                </div>
+
+            </div>
+        </section>
+        <!-- End Team Section -->
+        
+        <!-- ======= Contact Section ======= -->
+        <section id="contact" class="contact section-bg">
+            <div class="container">
+
+                <div class="section-title">
+                    <h2>Contact Us</h2>
+                    
+                </div>
+
+                <div class="row">
+
+                    <div class="col-lg-4">
+                        <div class="info d-flex flex-column justify-content-center" data-aos="fade-right">
+                            <div class="address">
+                                <i class="bi bi-geo-alt"></i>
+                                <h4>Location:</h4>
+                                <p>3341 Regent Blvd,<br>Ste 130 118 Irving, TX 75063</p>
+                            </div>
+
+                            <div class="email">
+                                <i class="bi bi-envelope"></i>
+                                <h4>Email:</h4>
+                                <p>info@mindspaceit.com</p>
+                            </div>
+
+                            <div class="phone">
+                                <i class="bi bi-phone"></i>
+                                <h4>Call:</h4>
+                                <p>+1 (346) 334-1786</p>
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                    <div class="col-lg-8 mt-5 mt-lg-0">
+
+                        <form action="forms/contact.php" method="post" role="form" class="php-email-form" data-aos="fade-left">
+                            <div class="row">
+                                <div class="col-md-6 form-group">
+                                    <input type="text" name="cname" class="form-control" id="cname" placeholder="Your Name" required>
+                                </div>
+                                <div class="col-md-6 form-group mt-3 mt-md-0">
+                                    <input type="email" class="form-control" name="cemail" id="cemail" placeholder="Your Email" required>
+                                </div>
+                            </div>
+                            <div class="form-group mt-3">
+                                <input type="text" class="form-control" name="subject" id="subject" placeholder="Subject" required>
+                            </div>
+                            <div class="form-group mt-3">
+                                <textarea class="form-control" name="cmessage" id="cmessage" rows="5" placeholder="Message" required></textarea>
+                            </div>
+                            <div class="text-center"><button type="submit">Send Message</button></div>
+                        </form>
+
+                    </div>
+
+                </div>
+
+            </div>
+        </section>
+        <!-- End Contact Section -->
+
+    </main>
+    <!-- End #main -->
+
+    <!-- ======= Footer ======= -->
+    <?php include("footer.php"); ?>
+    <!-- End Footer -->
+
+    <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+    <!-- Vendor JS Files -->
+    <script src="assets/vendor/aos/aos.js"></script>
+    <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+    <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+    <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+    <script src="assets/vendor/php-email-form/validate.js"></script>
+
+    <!-- Template Main JS File -->
+    <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
